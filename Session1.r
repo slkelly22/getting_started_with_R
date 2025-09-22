5849eb5d24dd9ca47f990f74d6dcd8aea9f3acf4
slkelly22 <slkelly@go.olemiss.edu>
2025-09-22 14:27:33 -0500

    quick demo in session

---
+++ Session1.r
@@ -0,0 +1,15 @@
+# Getting Started with R
+# Session 1
+
+# getwd tells you where your working directory is located
+getwd()
+
+# I am creating an object
+apples <- c("red", "green", "red")
+apples
+apples <- c(2, 4, 5)
+apples
+
+?c
+?range
+args(range)
